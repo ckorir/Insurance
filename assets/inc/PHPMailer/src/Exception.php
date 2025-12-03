2a8907859f7e908e7693402c6f4f9beac156c299
ckorir <chrisruto.k@gmail.com>
2025-12-03 19:31:01 +0300

    Made Policy form work

--- assets/inc/PHPMailer/src/Exception.php
+++ assets/inc/PHPMailer/src/Exception.php
@@ -3,10 +3,29 @@
 /**
  * PHPMailer Exception class.
  * PHP Version 5.5.
+ *
+ * @see       https://github.com/PHPMailer/PHPMailer/ The PHPMailer GitHub project
+ *
+ * @author    Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
+ * @author    Jim Jagielski (jimjag) <jimjag@gmail.com>
+ * @author    Andy Prevost (codeworxtech) <codeworxtech@users.sourceforge.net>
+ * @author    Brent R. Matzelle (original founder)
+ * @copyright 2012 - 2020 Marcus Bointon
+ * @copyright 2010 - 2012 Jim Jagielski
+ * @copyright 2004 - 2009 Andy Prevost
+ * @license   https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html GNU Lesser General Public License
+ * @note      This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 namespace PHPMailer\PHPMailer;
 
+/**
+ * PHPMailer exception handler.
+ *
+ * @author Marcus Bointon <phpmailer@synchromedia.co.uk>
+ */
 class Exception extends \Exception
 {
     /**
@@ -16,6 +35,6 @@ class Exception extends \Exception
      */
     public function errorMessage()
     {
-        return '<strong>' . htmlspecialchars($this->getMessage(), ENT_COMPAT, 'UTF-8') . "</strong><br />\n";
+        return '<strong>' . htmlspecialchars($this->getMessage(), ENT_COMPAT | ENT_HTML401) . "</strong><br />\n";
     }
 }
--- assets/inc/sendemail.php
+++ assets/inc/sendemail.php
@@ -1,75 +1,74 @@
 <?php
-header('Content-Type: application/json');
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+// Load PHPMailer classes
+require __DIR__ . '/PHPMailer/src/PHPMailer.php';
+require __DIR__ . '/PHPMailer/src/SMTP.php';
+require __DIR__ . '/PHPMailer/src/Exception.php';
 
-// Collect POST data
-$applicant_name = $_POST['applicant_name'] ?? '';
-$email = $_POST['email'] ?? '';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-if(empty($applicant_name) || empty($email)){
-    echo json_encode(["status"=>"error","message"=>"Full name and email are required."]);
-    exit;
+    // Collect all form fields
+    $city = $_POST['city'] ?? '';
+    $applicant_name = $_POST['applicant_name'] ?? '';
+    $next_of_kin = $_POST['next_of_kin'] ?? '';
+    $id_number = $_POST['id_number'] ?? '';
+    $dob = $_POST['dob'] ?? '';
+    $address = $_POST['address'] ?? '';
+    $contact_no = $_POST['contact_no'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $occupation = $_POST['occupation'] ?? '';
+    $monthly_income = $_POST['monthly_income'] ?? '';
+    $plan = $_POST['plan'] ?? '';
+    $terms = $_POST['terms'] ?? '';
+    $physical_impairment = $_POST['physical_impairment'] ?? '';
+    $medical_history = $_POST['medical_history'] ?? '';
+    $current_health = $_POST['current_health'] ?? '';
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP configuration
+        $mail->isSMTP();
+        $mail->Host       = 'mail.primecoverins.co.ke';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'info@primecoverins.co.ke';
+        $mail->Password   = 'infopassword123'; // Replace with actual password
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+
+        // Email headers
+        $mail->setFrom('info@primecoverins.co.ke', 'Prime Cover Insurance');
+        $mail->addAddress('info@primecoverins.co.ke'); // Send to yourself
+
+        $mail->isHTML(true);
+        $mail->Subject = 'New Policy Proposal Submitted';
+
+        // Construct HTML email body
+        $mail->Body = "
+        <h2>New Policy Proposal Submission</h2>
+        <b>City:</b> $city<br>
+        <b>Applicant Name:</b> $applicant_name<br>
+        <b>Next of Kin:</b> $next_of_kin<br>
+        <b>ID Number:</b> $id_number<br>
+        <b>Date of Birth:</b> $dob<br>
+        <b>Address:</b> $address<br>
+        <b>Contact No:</b> $contact_no<br>
+        <b>Email:</b> $email<br>
+        <b>Occupation:</b> $occupation<br>
+        <b>Monthly Income:</b> $monthly_income<br>
+        <b>Plan:</b> $plan<br>
+        <b>Terms:</b> $terms<br>
+        <b>Physical Impairment:</b> $physical_impairment<br>
+        <b>Medical History:</b> $medical_history<br>
+        <b>Current Health:</b> $current_health
+        ";
+
+        $mail->send();
+        echo json_encode(['status' => 'success', 'message' => 'Proposal submitted successfully!']);
+    } catch (Exception $e) {
+        echo json_encode(['status' => 'error', 'message' => "Mailer Error: {$mail->ErrorInfo}"]);
+    }
 }
-
-// Other fields
-$city = $_POST['city'] ?? '';
-$next_of_kin = $_POST['next_of_kin'] ?? '';
-$id_number = $_POST['id_number'] ?? '';
-$dob = $_POST['dob'] ?? '';
-$address = $_POST['address'] ?? '';
-$contact_no = $_POST['contact_no'] ?? '';
-$occupation = $_POST['occupation'] ?? '';
-$monthly_income = $_POST['monthly_income'] ?? '';
-$plan = $_POST['plan'] ?? '';
-$terms = $_POST['terms'] ?? '';
-$physical_impairment = $_POST['physical_impairment'] ?? '';
-$medical_history = $_POST['medical_history'] ?? '';
-$current_health = $_POST['current_health'] ?? '';
-
-// PHPMailer setup
-$mail = new PHPMailer(true);
-
-try {
-    $mail->isSMTP();
-    $mail->Host       = 'mail.primecoverins.co.ke';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'info@primecoverins.co.ke';
-    $mail->Password   = 'infopassword123';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-
-    $mail->setFrom('info@primecoverins.co.ke', 'Prime Cover Insurance');
-    $mail->addAddress('info@primecoverins.co.ke'); // receiver
-
-    $mail->isHTML(true);
-    $mail->Subject = 'New Policy Proposal';
-    $mail->Body    = "
-        <h2>Policy Proposal Submission</h2>
-        <p><strong>Name:</strong> $applicant_name</p>
-        <p><strong>Email:</strong> $email</p>
-        <p><strong>City:</strong> $city</p>
-        <p><strong>Next of Kin:</strong> $next_of_kin</p>
-        <p><strong>ID No:</strong> $id_number</p>
-        <p><strong>DOB:</strong> $dob</p>
-        <p><strong>Address:</strong> $address</p>
-        <p><strong>Contact:</strong> $contact_no</p>
-        <p><strong>Occupation:</strong> $occupation</p>
-        <p><strong>Monthly Income:</strong> $monthly_income</p>
-        <p><strong>Plan:</strong> $plan</p>
-        <p><strong>Terms:</strong> $terms</p>
-        <p><strong>Physical Impairment:</strong> $physical_impairment</p>
-        <p><strong>Medical History:</strong> $medical_history</p>
-        <p><strong>Current Health:</strong> $current_health</p>
-    ";
-
-    $mail->send();
-    echo json_encode(["status"=>"success","message"=>"Proposal submitted successfully!"]);
-} catch (Exception $e) {
-    echo json_encode(["status"=>"error","message"=>"Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
-}
+?>
